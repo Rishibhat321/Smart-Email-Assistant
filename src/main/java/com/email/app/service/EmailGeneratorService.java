a0eedd6eb28140a5fadf3e1a5592d3e1a9dfec9c
Rishib#321 <rishitab2209@gmail.com>
2025-03-03 03:10:44 +0530

    added controller and dto and service layer.

--- src/main/java/com/email/app/service/EmailGeneratorService.java
+++ src/main/java/com/email/app/service/EmailGeneratorService.java
@@ -1,15 +1,48 @@
 package com.email.app.service;
 
 import com.email.app.dto.EmailRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class EmailGeneratorService {
 
-    public String generateEmailReply(EmailRequest emailRequest) {
+    @Value("${gemini.api.url}")
+    private String geminiApiUrl;
 
+    @Value("${gemini.api.key}")
+    private String geminiApiKey;
+
+    public String generateEmailReply(EmailRequest emailRequest) {
         // Build the prompt
+        String prompt = buildPrompt(emailRequest);
+
         // craft a request
+        Map<String, Object> requestBody = Map.of(
+                "contents", new Object[] {
+                     Map.of("parts", new Object[] {
+                             Map.of("text", prompt)
+                     })
+                }
+        );
+
+        // Do request and get response (we will need the API Key)
+
+        // Return response
 
     }
+
+    private String buildPrompt(EmailRequest emailRequest) {
+        StringBuilder prompt = new StringBuilder();
+        prompt.append("Generate a professional email reply for the following email content. Please don't generate a subject line ");
+
+        if(emailRequest.getTone() != null && !emailRequest.getTone().isEmpty()) {
+            prompt.append("Use a ").append(emailRequest.getTone()).append("tone.");
+        }
+        prompt.append("\nOriginal email : \n").append(emailRequest.getEmailContent());
+        return prompt.toString();
+    }
+
 }
