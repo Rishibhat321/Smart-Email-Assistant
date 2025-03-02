07d401e5a62a3995c4e7cfb259ec6cc1421a2c2d
Rishib#321 <rishitab2209@gmail.com>
2025-03-03 03:31:20 +0530

    added controller and dto and service layer.

--- src/main/java/com/email/app/controller/EmailGeneratorController.java
+++ src/main/java/com/email/app/controller/EmailGeneratorController.java
@@ -1,7 +1,10 @@
 package com.email.app.controller;
 
 import com.email.app.dto.EmailRequest;
+import com.email.app.service.EmailGeneratorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/email")
 public class EmailGeneratorController {
 
-    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest) {
-        return ResponseEntity.ok("");
+    private final EmailGeneratorService emailGeneratorService;
+
+    @Autowired
+    public EmailGeneratorController(EmailGeneratorService emailGeneratorService) {
+        this.emailGeneratorService = emailGeneratorService;
     }
+
+    @PostMapping("/generate")
+    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest) {
+        String response = emailGeneratorService.generateEmailReply(emailRequest);
+        return ResponseEntity.ok(response);
+    }
+
 }
